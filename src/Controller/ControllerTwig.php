3d3ae59f797b2b76a3200d39aabd4fd4c232cf2a
Tove Lernvall <tove.lernvall@gmail.com>
2023-04-12 09:43:07 +0200

    Updated for kmom02

--- src/Controller/ControllerTwig.php
+++ src/Controller/ControllerTwig.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 class ControllerTwig extends AbstractController
 {
     #[Route("/lucky", name: "lucky")]
@@ -49,25 +48,9 @@ class ControllerTwig extends AbstractController
         return $this->render('report.html.twig');
     }
 
-    #[Route("/api/quote", name: "api/quote")]
-    public function jsonNumber(): Response
+    #[Route("/api", name: "api")]
+    public function api(): Response
     {
-        date_default_timezone_set('Europe/Stockholm');
-        $number = random_int(0, 4);
-        $str = file_get_contents('json/quotes.json');
-        $json = json_decode($str, true);
-        $jsonquote = $json[$number];
-        $today = date("Y-m-d H:i:s");
-        $timestamp = time();
-
-        $data = [
-            'random number' => $number,
-            'random quote' => $jsonquote['quote'],
-            'date' => $today,
-            'timestamp' => $timestamp
-        ];
-
-        return new JsonResponse($data);
+        return $this->render('api.html.twig');
     }
 }
-
